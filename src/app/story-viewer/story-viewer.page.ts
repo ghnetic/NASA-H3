7873144623cac9395483dcd8736ae558a9928bc1
ghnetic <sofiagineth4@gmail.com>
2023-10-08 18:06:50 -0600

    Map

--- src/app/story-viewer/story-viewer.page.ts
+++ src/app/story-viewer/story-viewer.page.ts
@@ -1,36 +1,66 @@
 import {
-  AfterViewInit,
   Component,
-  ElementRef,
-  ViewChild,
 } from '@angular/core';
-import mapboxgl from 'mapbox-gl';
-import Map from 'mapbox-gl';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-story-viewer',
   templateUrl: './story-viewer.page.html',
   styleUrls: ['./story-viewer.page.scss'],
 })
-export class StoryViewerPage implements AfterViewInit {
+export class StoryViewerPage implements OnInit {
 
   slideOpts: any;
 
-  constructor(
-  ) { }
+  ip: string ='';
+  url: string='';
+  lat: any;
+  lon: any;
 
-
-  @ViewChild('mapDiv')
-  mapDivElement!: ElementRef
-
-  ngAfterViewInit(): void {
-    mapboxgl.accessToken = 'pk.eyJ1Ijoic3ZpZGVhIiwiYSI6ImNsbmh5dnBiaTE1bHgybnBlemE0cWpkYnMifQ.-wQTKAiiXOXJf3vZL8qI3g';
-    const map = new mapboxgl.Map({
-      container: this.mapDivElement.nativeElement,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-74.5, 40],
-      zoom: 14
-    })
+  ngOnInit() {
+  this.getWeather();
   }
+
+  goToHome() {
+    this.router.navigate(['explore']);
+  }
+
+  constructor(private router: Router){
+
+  }
+
+  getWeather(){
+    //Aqui obtenemos el ip
+          fetch('https://api.my-ip.io/ip.json')
+          .then(response=> response.json())
+          .then(async data => {
+            this.ip=data.ip.toString();
+            //console.log(this.ip);
+
+            this.url='https://weatherapi-com.p.rapidapi.com/ip.json?q='+this.ip;
+            const options = {
+              method: 'GET',
+              headers: {
+                'X-RapidAPI-Key': '3b54501c1cmsh9bf1440086b89cfp1fc3f9jsn1d3de34c91a2',
+                'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+              }
+            };
+            try {
+              const response = await fetch(this.url, options);
+              const result = await response.text();
+
+              this.lat= JSON.stringify(result);
+              console.log(this.lat);
+
+              //console.log('hola');
+            } catch (error) {
+              console.log('adios');
+              console.error(error);
+            }
+        });
+
+
+    }
 
 }
